51c1ae893096373e7bc80150bbbef20767fffaa1
CharlesMich <charleedongo@hotmail.com>
2024-12-03 17:53:33 -0600

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Charles Michael on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
